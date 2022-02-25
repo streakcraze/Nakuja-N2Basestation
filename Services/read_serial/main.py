926ca7cea0314a59fc2ff0a1b5fc7ba5faa51b7d
0x6f736f646f <b1ackd0t@protonmail.com>
2022-02-25 22:32:43 +0000

    :zap: modularisation

--- Services/read_serial/main.py
+++ Services/read_serial/main.py
@@ -1,49 +1,28 @@
 import serial
+import os
 import time
 import queue
 import threading
 import logging
 import numpy as np
 from datetime import datetime
+from lib import setup_logging
 
-# Values are in seconds
-RECORD_DURATION = 5
-TOTAL_DURATION = 60
+# Setup as environment variable
+WRITE_FILE_DURATION=5
+ESTIMATED_DURATION=60
 
 class FetchData:
 
-    def __init__(self):
-        # TODO implement cython arrays to boost the efficiency
-        self.setup_logging()
-        try:
-            self.serial = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, parity=serial.PARITY_NONE,
-                stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
-        except Exception as e:
-            self.logger.error(e)
-            raise e
-
+    def __init__(self, logger):
+        self.logger = logger
+        self.serial = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
         self.publisher = None
         self.stopped = False
         self.dataqueue = queue.Queue()
+        # TODO implement cython arrays to boost the efficiency
         self.datapoints = []
-
-    def setup_logging(self):
-        # TODO add in utils
-        self.logger = logging.getLogger('FETCHDATA')
-        self.logger.setLevel(logging.INFO)
-
-        fileHandler = logging.FileHandler('readserial.log')
-        fileHandler.setLevel(logging.INFO)
-
-        consoleHandler = logging.StreamHandler()
-        consoleHandler.setLevel(logging.INFO)
-
-        self.logger.addHandler(fileHandler)
-        self.logger.addHandler(consoleHandler)
-
-        formatter = logging.Formatter('%(asctime)s  %(name)s  %(levelname)s: %(message)s')
-        consoleHandler.setFormatter(formatter)
-        fileHandler.setFormatter(formatter)
+        self.cwd = os.path.dirname(__file__)
         
     def save_data(self):
         self.logger.info("Started saving data thread")
@@ -51,39 +30,41 @@ class FetchData:
             time.sleep(1)
             while not self.dataqueue.empty():
                 datapoints = self.dataqueue.get()
-                filename = datetime.now().strftime("%A_%d_%B_%Y_%H_%M_%S") + ".csv"
+                filename = str(self.cwd) + "/" + datetime.now().strftime("%A_%d_%B_%Y_%H_%M_%S") + ".csv"
                 np.savetxt(filename, datapoints, newline="\n", fmt='%s')
-                self.logger.info("Saved batched data")
+                self.logger.debug("Saved batched data")
 
     def read_data(self):
         self.logger.info("Started reading data thread")
         while True:
             start_time = time.time()
             elapsed_time = 0
-            while elapsed_time <= RECORD_DURATION:
+            while elapsed_time <= WRITE_FILE_DURATION:
                 val = self.serial.readline().decode('utf-8').rstrip()
                 self.datapoints.append(val)
                 elapsed_time = time.time() - start_time
             self.dataqueue.put(self.datapoints)
             self.datapoints = []
-            self.logger.info("Sent batched data to queue")
+            self.logger.debug("Sent batched data to queue")
             if self.stopped:
                 return
     
     def run(self):
         self.logger.info("Started reading serial service")
-        self.savethread = threading.Thread(target=self.save_data)
         self.readthread = threading.Thread(target=self.read_data)
-        self.savethread.setDaemon(True)
+        self.savethread = threading.Thread(target=self.save_data)
         self.readthread.setDaemon(True)
+        self.savethread.setDaemon(True)
         self.readthread.start()
         self.savethread.start()
         self.readthread.join()
         
     def stop(self):
+        self.logger.info("Stopped Service")
         self.stopped = True
 
-fetch_data = FetchData()
-fetch_data.run()
-time.sleep(TOTAL_DURATION)
-fetch_data.stop()
+def main(logger):
+    fetch_data = FetchData(logger = logger)
+    fetch_data.run()
+    time.sleep(ESTIMATED_DURATION)
+    fetch_data.stop()
